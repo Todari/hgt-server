30de32d04f05c5c35ba547cc65375a374d1f468f
hyungu1215 <hyungu1215@naver.com>
2023-09-17 21:07:55 +0900

    add "aes256" funcs

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"bytes"
 	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
 	"fmt"
 	"github.com/Todari/hgt-server/models"
 	"github.com/Todari/hgt-server/services"
@@ -93,6 +97,66 @@ func SignIn() gin.HandlerFunc {
 			},
 		)
 	}
+}
+
+type cryptoKeys struct {
+	cipherKey   string
+	cipherIvKey string
+}
+
+func (c cryptoKeys) encrypter(plainText string) (string, error) {
+	if strings.TrimSpace(plainText) == "" {
+		return plainText, nil
+	}
+
+	block, err := aes.NewCipher([]byte(c.cipherIvKey))
+	if err != nil {
+		return "", err
+	}
+
+	encrypter := cipher.NewCBCEncrypter(block, []byte(c.cipherIvKey))
+	paddedPlainText := padPKCS7([]byte(plainText), encrypter.BlockSize())
+
+	cipherText := make([]byte, len(paddedPlainText))
+	// CryptBlocks 함수에 데이터(paddedPlainText)와 암호화 될 데이터를 저장할 슬라이스(cipherText)를 넣으면 암호화가 된다.
+	encrypter.CryptBlocks(cipherText, paddedPlainText)
+
+	return base64.StdEncoding.EncodeToString(cipherText), nil
+}
+
+func (c cryptoKeys) decrypt(cipherText string) (string, error) {
+	if strings.TrimSpace(cipherText) == "" {
+		return cipherText, nil
+	}
+
+	decodedCipherText, err := base64.StdEncoding.DecodeString(cipherText)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher([]byte(c.cipherKey))
+	if err != nil {
+		return "", err
+	}
+
+	decrypter := cipher.NewCBCDecrypter(block, []byte(c.cipherIvKey))
+	plainText := make([]byte, len(decodedCipherText))
+
+	decrypter.CryptBlocks(plainText, decodedCipherText)
+	trimmedPlainText := trimPKCS5(plainText)
+
+	return string(trimmedPlainText), nil
+}
+
+func padPKCS7(plainText []byte, blockSize int) []byte {
+	padding := blockSize - len(plainText)%blockSize
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(plainText, padText...)
+}
+
+func trimPKCS5(text []byte) []byte {
+	padding := text[len(text)-1]
+	return text[:len(text)-int(padding)]
 }
 
 func SignOut(ctx *gin.Context) {
